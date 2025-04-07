cdcfaa9dfe3695e6f8feee885710eca4eab484e9
DeniseNunesGH <selineliine@gmail.com>
2025-04-07 01:32:21 -0300

    Update Hotel.kt

--- src/Hotel.kt
+++ src/Hotel.kt
@@ -8,20 +8,28 @@ fun main() {
     println("Insira o nome de usuário: ")
     usuario = readln()
 
-    println("Insira sua senha: ")
+    println("Olá, $usuario. Insira sua senha: ")
     val senha = readln().toInt() //2.
 
     if (senha != 2678) {
         println("Senha incorreta. Tente novamente!")
         main()
     } else {
-        print("Bem vindo ao Habbo! $usuario\n")
+        print("Bem vindo ao Habbo! $usuario. É um prazer imenso ter você por aqui!.\n")
         iniciar()
     }
 }
 
 fun iniciar() {
-    println("Escolha uma opção:")
+    println("""Escolha uma opção:
+        |1. Cadastro de Quartos
+        |2. Cadastro de Hóspedes
+        |3. Meia entrada e gratuidade
+        |4. Evento
+        |5. Estacionamento
+        |6. Manuteção
+        |7. Sair
+    """.trimMargin())
     // A varival escolha armazena a opção escolhida pelo usuário.
     // uma variavel local é utilizada apenas dentro da função inicio().
     val escolha = readln().toIntOrNull()
@@ -30,14 +38,15 @@ fun iniciar() {
         2 -> cadastrarHospedes()
         3 -> comoSoletra()
         4 -> Evento()
-        4 -> abastecimentoDeAutomoveis()
-        5 -> sairDoHotel()
+        5 -> abastecimentoDeAutomoveis()
+        6 -> arCondicionado()
+        7 -> sairDoHotel()
         else -> erro()
     }
 }
 
 fun sairDoHotel() {
-    println("Você deseja sair?\n Informe 1 para \"sim\" e 0 para \"não\"")
+    println("Você deseja sair do Habbo?\n Informe 1 para \"sim\" e 0 para \"não\"")
     val confirma = readln().toBoolean() // digite true ou false ou 1 ou 0
     if (confirma) {
         println("Até logo!")
